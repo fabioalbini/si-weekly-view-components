2994a5bebd35f0f2ae1c8a48937e468b6737eec4
Fábio Albini Braga <fabio.albini@diffagency.com>
2022-12-08 14:48:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+Task: Complete the spec to visualize the products detail page
+
+spec/system/products/product_details_spec.rb
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class ProductsController < ApplicationController
+  def index
+    @products = 25.times.map do |i|
+      new_product(id: i, name: "Product #{i}")
+    end
+
+    @query = params[:query]
+    @products = @products.select { |p| p.name.include?(@query) } if @query.present?
+  end
+
+  def show
+    @product = new_product(
+      id: params[:id],
+      name: "Product #{params[:id]}",
+      description: "Description #{params[:id]}"
+    )
+  end
+
+  private
+
+  def new_product(id:, name:, description: '')
+    OpenStruct.new(id: id, name: name, description: description)
+  end
+end
